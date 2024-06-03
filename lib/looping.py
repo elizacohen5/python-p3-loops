c6f11b3a96fabf35cadd4e11010b5f6196935a8c
Eliza Cohen <elizacohen5@gmail.com>
2024-06-03 15:35:54 -0400

    complete python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i <= 10 and i >= 0:
+        if i == 0:
+            print("Happy New Year!")
+        else:
+            print(i)
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_int_list = []
+    for int in int_list:
+        squared_int_list.append(int * int)
+    # print(squared_int_list)
+    return squared_int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i >= 1 and i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+
+# happy_new_year()
+# square_integers([1, 2, 3, 4, 5])
+fizzbuzz()
+
